ca473cbf5b50ec0e2820c78be34f311fb7ece06f
jaimey1983 <jeg19837@gmail.com>
2019-10-14 20:35:51 -0500

    pequeño cambio

--- index.php
+++ index.php
@@ -6,35 +6,35 @@
 </head>
 
 <body>
-	
+
 	<?php
-		
-		// Esto es un comentario
+
+		// Esto es un nuevo comentario
 		# Esto también es un comentario.
 		/* Esto es un comentario más largo. En realidad es para strings largos, verdaderamente largos. Entonces nos muestra en varias líneas los comentarios extensos que hagamos en el documento */
-	
+
 		echo "Hola mundo, este es mi primer hola mundo con ED TEAM"."\n"."hola jaime </p>";
 		// Con el punto se unen varias cadenas de strings.
 		/* Aunque para mí el lenguaje de php no es ajeno siempre se me olvidan las etiquetas para abrir y cerrar php. (<?php ?>) */
 		print("Hola mundo <br>");
-	
+
 		//VARIABLES
 		/*Una variable es una pequeña caja que guarda una información, pueden ser strins o enteros(valores numéricos)*/
-	
+
 		$welcome = "Hola mundo... <br>"; //Esta es la primera variable hecha en este archivo.
 		echo $welcome;
-		
-		
+
+
 		//FUNCIONES
-	
+
 		function suma($a=3, $b=5){
 			$suma= $a+$b;
 			return $suma;
 		}
 		echo suma();
-	
+
 		echo "<br>";
-	
+
 		function Suma2($c=24, $d=35){
 			if($c+$d<=48){
 				echo "verdadero";
@@ -47,8 +47,8 @@
 			}
 		}
 		echo Suma2();
-	
+
 	?>
-	
+
 </body>
 </html>
